c2f5ddc8f7ae44b5514eb5efd0984af850117080
LEONARDO ALBERTO ESCOBAR PAREDES <20000453@alumnos.utleon.edu.mx>
2023-05-31 01:47:05 -0600

    Implementación de la conexión a la base de datos de SQL Server y entity para el manejo desde C# con cshtml y sus respectivos controller y modelos para su correcta gestión

---
+++ IDGS903_BD/Models/AlumnosDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace IDGS903_BD.Models
+{
+	public class AlumnosDbContext:DbContext
+	{
+		private static string connection = "DefaultConnection";
+		public AlumnosDbContext() : base(connection) { }
+		public DbSet <Alumnos> Alumnos { get; set; }
+
+	}
+}
